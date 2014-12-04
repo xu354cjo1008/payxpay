669daede7a20507cac348671ea6c53f59d9a3b89
xu354cjo1008 <xu354cjo1008@gmail.com>
2014-12-04 13:11:46 +0800

    sql modify 6

--- MYSQL_info.php
+++ MYSQL_info.php
@@ -2,10 +2,10 @@
 
     $url=parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-    $dbhost = 'us-cdbr-iron-east-01.cleardb.net'//$url["host"];
-    $dbuser = 'bae9e968e129dc'//$url["user"];
-    $dbpass = 'e47e9189'//$url["pass"];
-    $dbname = 'heroku_3ae224a4e4a2b87'//substr($url["path"],1);
+    $dbhost = $url["host"];
+    $dbuser = $url["user"];
+    $dbpass = $url["pass"];
+    $dbname = substr($url["path"],1);
 
     mysqli_connect($dbhost, $dbuser, $dbpass);
 
